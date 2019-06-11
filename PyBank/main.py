5192bcda6e96ed94260ce3fe3fcdb3d5b7fc2efb
Hanna Polowczyk <hapolowczyk@kobo.com>
2019-06-11 10:33:05 -0400

    Added comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,12 +1,13 @@
 # PyBank Assignment
 
-#Modules
+# Import modules
 import os
 import csv
 
 # Set path for file
 bdgt_path = os.path.join("..", "PyBank", "budget_data.csv")
 
+# Set empty lists
 months = []
 p_l = []
 
@@ -15,17 +16,27 @@ with open(bdgt_path, newline="", encoding="utf8") as bdgt_file:
     csv_reader = csv.reader(bdgt_file, delimiter=",")
     csv_header=next(csv_reader)
     for row in csv_reader:
+        # Put all months into a list
         months.append(row[0])
+        # Put all profit/loss values into a list
         p_l.append(int(row[1]))
+        # Create a list that determines the difference between two subsequent profit/loss values
         diff = [p_l[i]-p_l[i-1] for i in range(1,len(p_l))]
 
+    # Sum all of the profit/loss values
     total_p_l = sum(p_l)
+    # Determine the average of the differences between the profit/loss values
     ave_change = round(sum(diff)/len(diff),2)
+    # Determine the largest difference between subsequent profit/loss values
     max_change = max(diff)
+    # Determine the smallest difference between subsequent profit/loss values
     min_change = min(diff)
+    # Determine the month where the largest difference occurs
     max_month = months[diff.index(max_change) + 1]
+    # Determine the month where the smallest difference occurs
     min_month = months[diff.index(min_change) + 1]
 
+# To print in Terminal
 print(f"Financial Analysis")
 print(f"-----------------------------------")
 print(f"Total Months: {len(months)}")
@@ -34,7 +45,8 @@ print(f"Average Change: ${ave_change}")
 print(f"Greatest Increase in Profits: {max_month} (${max_change})")
 print(f"Greatest Decrease in Profits: {min_month} (${min_change})")
 
-f = open("Pybank.txt","w")
+# To print in seperate .txt file named PyBank
+f = open("PyBank.txt","w")
 print(f"Financial Analysis", file=f)
 print(f"-----------------------------------", file=f)
 print(f"Total Months: {len(months)}", file=f)
@@ -42,12 +54,3 @@ print(f"Total: ${total_p_l}", file=f)
 print(f"Average Change: ${ave_change}", file=f)
 print(f"Greatest Increase in Profits: {max_month} (${max_change})", file=f)
 print(f"Greatest Decrease in Profits: {min_month} (${min_change})", file=f)
-
-
-
-    
-
-
-    
-
-
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,58 +1,61 @@
 # PyPoll Assignment 
 
-#Modules
+# Import modules
 import os
 import csv
 
 # Set path for file
 elec_path = os.path.join("..", "PyPoll", "election_data.csv")
-# Set empty candidate list
-candidate = []
+
+# Set empty candidate votes list
+candidate_votes = []
 
 # Open the CSV
 with open(elec_path, newline="", encoding="utf8") as elec_file:
     csv_reader = csv.reader(elec_file, delimiter=",")
     csv_header=next(csv_reader)
-    # Create list of 
     for row in csv_reader:
-       candidate.append(row[2])
+       # Put all candidate votes into a list
+       candidate_votes.append(row[2])
     
     # Determine unique canditates
-    cand_list = [cand for cand in set(candidate)]
+    candidate_list = [candidate for candidate in set(candidate_votes)]
     # Determine the number of votes per candidate
-    vote_total =[candidate.count(i) for i in cand_list]
+    votes_per_cand =[candidate_votes.count(vote) for vote in candidate_list]
     # Determine total number of votes
-    total_votes = len(candidate)
+    total_votes = len(candidate_votes)
     # Determine the percentage of votes per candidate
-    vote_percent = [round(votes/total_votes,2) for votes in vote_total]
+    vote_percent = [round(votes/total_votes,2) for votes in votes_per_cand]
     # Combine number of votes and percentage of votes per candidate in seperate lists
-    vote_list = list(zip(vote_total,vote_percent))
+    vote_list = list(zip(votes_per_cand,vote_percent))
     # Create a dictionary with each candidate as a key, and vote_list as its respective values
-    cand_dict = dict(zip(cand_list,vote_list))
+    candidate_dict = dict(zip(candidate_list,vote_list))
     # Determine winner by using max
-    winner = max(cand_dict,key=cand_dict.get)
+    winner = max(candidate_dict, key=candidate_dict.get)
 
+# To print in Terminal
 print(f"Election Results")
 print(f"-----------------------------------")
 print(f"Total Votes: {total_votes}")
 print(f"-----------------------------------")
-print(f"Khan: "+"{:.3%}".format(cand_dict['Khan'][1])+f" ({cand_dict['Khan'][0]})")
-print(f"Correy: "+"{:.3%}".format(cand_dict['Correy'][1])+f" ({cand_dict['Correy'][0]})")
-print(f"Li: "+"{:.3%}".format(cand_dict['Li'][1])+f" ({cand_dict['Li'][0]})")
-print(f"O'Tooley: "+"{:.3%}".format(cand_dict["O'Tooley"][1]) + " ("+ str(cand_dict["O'Tooley"][0]) +")")
+print(f"Khan: "+"{:.3%}".format(candidate_dict['Khan'][1])+f" ({candidate_dict['Khan'][0]})")
+print(f"Correy: "+"{:.3%}".format(candidate_dict['Correy'][1])+f" ({candidate_dict['Correy'][0]})")
+print(f"Li: "+"{:.3%}".format(candidate_dict['Li'][1])+f" ({candidate_dict['Li'][0]})")
+print(f"O'Tooley: "+"{:.3%}".format(candidate_dict["O'Tooley"][1]) + " ("+ str(candidate_dict["O'Tooley"][0]) +")")
 print(f"-----------------------------------")
 print(f"Winner: {winner}")
 print(f"-----------------------------------")
 
+# To print in seperate .txt file named PyPoll
 f = open("PyPoll.txt","w")
 print(f"Election Results", file=f)
 print(f"-----------------------------------", file=f)
 print(f"Total Votes: {total_votes}", file=f)
 print(f"-----------------------------------", file=f)
-print(f"Khan: "+"{:.3%}".format(cand_dict['Khan'][1])+f" ({cand_dict['Khan'][0]})", file=f)
-print(f"Correy: "+"{:.3%}".format(cand_dict['Correy'][1])+f" ({cand_dict['Correy'][0]})", file=f)
-print(f"Li: "+"{:.3%}".format(cand_dict['Li'][1])+f" ({cand_dict['Li'][0]})", file=f)
-print(f"O'Tooley: "+"{:.3%}".format(cand_dict["O'Tooley"][1]) + " ("+ str(cand_dict["O'Tooley"][0]) +")", file=f)
+print(f"Khan: "+"{:.3%}".format(candidate_dict['Khan'][1])+f" ({candidate_dict['Khan'][0]})", file=f)
+print(f"Correy: "+"{:.3%}".format(candidate_dict['Correy'][1])+f" ({candidate_dict['Correy'][0]})", file=f)
+print(f"Li: "+"{:.3%}".format(candidate_dict['Li'][1])+f" ({candidate_dict['Li'][0]})", file=f)
+print(f"O'Tooley: "+"{:.3%}".format(candidate_dict["O'Tooley"][1]) + " ("+ str(candidate_dict["O'Tooley"][0]) +")", file=f)
 print(f"-----------------------------------", file=f)
 print(f"Winner: {winner}", file=f)
 print(f"-----------------------------------", file=f)
